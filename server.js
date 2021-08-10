4ebd1259c14e6e4cc3ec0f254962aa894aff54fe
sclo00ps <louginescott@gmail.com>
2021-08-10 13:02:43 -0700

    added / html route to server.js file

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
+
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/google_book_search",
